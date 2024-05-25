311dcc57ab5a0443dcae846744c9a4350db94ce7
borasaygac <Bora.Saygac@campus.lmu.de>
2024-05-25 17:20:52 +0200

    added comments

--- variable.cpp
+++ variable.cpp
@@ -1,4 +1,5 @@
 #include "variable.hpp"
+#include <cstdlib>
 
 Variable::Variable(){ // constructor
     this -> pos_occ = 0;
@@ -10,40 +11,40 @@ Variable::~Variable(){ // destructor
 
 }
 
-void Variable::setIndex(int i) {
+void Variable::setIndex(int i) { // set the index of the variable (1-indexed to n= numOfVars) 
     this -> index = i;
 }
 
 // Number of occurance based operations
 
-void Variable::updateTotOcc() {
+void Variable::updateTotOcc() {  // update the total occurance of the variable
     this -> tot_occ = pos_occ + neg_occ;
 } 
 
-void Variable::incPosOcc() {
+void Variable::incPosOcc() { // increase the positive occurance of the variable
     this -> pos_occ++;
     updateTotOcc();
 } 
 
-void Variable::decPosOcc() {
+void Variable::decPosOcc() { // decrease the positive occurance of the variable
     this -> pos_occ--;
     updateTotOcc();
 } 
 
-void Variable::incNegOcc() {
+void Variable::incNegOcc() { // increase the negative occurance of the variable
     this -> neg_occ++;
     updateTotOcc();
 } 
 
-void Variable::decNegOcc() {
+void Variable::decNegOcc() { // decrease the negative occurance of the variable
     this -> neg_occ--;
     updateTotOcc();
 } 
 
-void Variable::addOccsToClause(int clause) {
+void Variable::addOccsToClause(int clause) { // adds which clauses the variable occurs in, positive or negative respectively 
     if (clause > 0) {
         occursPosInClauses.push_back(clause);
     } else {
-        occursNegInClauses.push_back(clause);
+        occursNegInClauses.push_back(abs(clause));
     }
 }
