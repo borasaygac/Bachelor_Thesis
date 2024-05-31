078f166fd67feca5c8faf2766d686ec513fc5fb9
borasaygac <Bora.Saygac@campus.lmu.de>
2024-05-31 17:07:34 +0200

    add rand horn generator

---
+++ randHornFormulaGenerator.py
@@ -0,0 +1,27 @@
+import random 
+
+def gen_rand_horn_formula(num_vars, num_clauses, max_clause_length): 
+    variables = [f'{i}' for i in range(1, num_vars + 1)]
+    formula = []
+
+    clause_lengths = list(range(1, min(num_vars, max_clause_length) + 1))
+    weights = [lenght*2 for lenght in clause_lengths]
+
+    for _ in range(num_clauses):
+        clause_length = random.choices(clause_lengths, weights=weights, k=1)[0] # Choose a weighted random clause length
+        clause_vars = random.sample(variables, k=clause_length) # at most 3 variables per clause
+        if clause_length > 1: # Randomly choose a positive or negative literal
+            positive_literal = random.choice(clause_vars)
+        else:
+            positive_literal = random.choice([random.choice(clause_vars), f'-{random.choice(clause_vars)}']) 
+        clause = ['0'] + [f' -{var}' if var != positive_literal else f' {var}' for var in clause_vars] # Negate all literals except the positive one
+        formula.append(''.join(clause))
+    
+    return '\n'.join(clause for clause in formula)
+
+num_vars = int(input("Enter the number of variables: "))
+num_clauses = int(input("Enter the number of clauses: "))
+max_clause_length = int(input("Enter the maximum clause length: "))
+
+print(f'p cnf {num_vars} {num_clauses}')
+print(gen_rand_horn_formula(num_vars, num_clauses, max_clause_length))
