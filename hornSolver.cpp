211f93b9c35fec8fbddc59d4b44e4347b4c13e51
borasaygac <Bora.Saygac@campus.lmu.de>
2024-06-05 18:50:50 +0200

    Move horn related funcs

--- hornSolver.cpp
+++ hornSolver.cpp
@@ -1,5 +1,46 @@
 #include "globals.hpp"
 
+void const printHornClauses(bool horn) {   // Print the Horn clauses for debugging purposes
+
+    if (!horn) {
+        printf("The formula is not Horn becasuse clause(s): ");
+        int hornCounter = 0;
+        for (int i = 1; i < numOfClauses + 1; i++){
+
+            if(!clauses[i].getIsHorn()) {
+                printf("%i ", i);
+                hornCounter++;
+            }
+        }
+        if (hornCounter == 1) {
+            printf("is not Horn.\n");
+        } else {
+            printf("are not Horn.\n");
+        }
+    } else {
+        printf("The formula is Horn because all clauses are Horn.\n");
+    }
+}
+
+
+bool const isHornFormula(int numOfClauses, vector<Clause> clauses){ // Check if the formula is Horn
+
+    int counter = 0;
+
+    for (int i = 1; i <= numOfClauses; i++) { // Check if all clauses are Horn
+        bool isClauseHorn = clauses[i].IsClauseHorn(i);
+        
+        if (isClauseHorn) counter++;
+    }
+
+    if (counter == numOfClauses) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 int hornSolver() {
 
     for (int i = 1; i <= numOfClauses; i++) {  // for each clause
